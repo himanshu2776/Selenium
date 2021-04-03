424ebb1711248ad8bd62be8222526da565726a31
himanshu2776 <himanshu.2776@gmail.com>
2021-04-03 13:58:35 +0530

    Update BrowserTest.java

--- src/main/java/BrowserTest.java
+++ src/main/java/BrowserTest.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BrowserTest {
 	
 	public static void main(String[] args) {
-	/*	String projectLocation = System.getProperty("user.dir");
+		String projectLocation = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", projectLocation+"\\libs\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.co.in");
-		driver.quit(); */
+		driver.quit(); 
 		
-		System.out.println("mmm");
+		
 	}
 
 }
